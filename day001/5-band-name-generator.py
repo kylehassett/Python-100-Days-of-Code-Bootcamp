42f88b3227a80169d4f7b50f59b228b4ec8646e9
Kyle Hassett <kylehassett@Kyles-MBP.lan>
2022-12-10 12:08:48 -0500

    Finished day 1, was very boring as I already know how to do everything of course, onto day 2

---
+++ day001/5-band-name-generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator!\n")
+
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your first pet?\n")
+
+print(f"Your band name is : {city} {pet}")
